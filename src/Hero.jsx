968caa7778d31f0713cb25f91001d0a01e529031
Brandon Yee <6111102+Firebird1029@users.noreply.github.com>
2022-01-24 00:31:08 -0500

    add icons

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -4,8 +4,14 @@ function Hero() {
 	return (
 		<section className="hero is-medium is-link">
 			<div className="hero-body">
-				<p className="title">Gather</p>
-				<p className="subtitle">Online Invitation Card Designer</p>
+				<p className="title">
+					Gather &nbsp;
+					<i className="fad fa-hands-heart" />
+				</p>
+				<p className="subtitle">
+					Online Invitation Card Designer &nbsp;
+					<i className="far fa-heart" />
+				</p>
 				<div classNameName="content">
 					With Gather, you can create your own personalized invitation cards in an instant! Choose your
 					favorite card design, enter your personalized text, and then download your card as a PDF. You also
